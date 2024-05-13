8c5713490d495ac70adebdca1e5abada39ec36c5
Gautam27021909 <kachhadiyagd@gmail.com>
2024-05-13 10:44:36 +0530

    update

--- hotel/management/urls.py
+++ hotel/management/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("error/",views.error, name="error"),
     path("about/", views.about, name= "about"),
     path("service/", views.service, name= "service"),
+    path("contact/", views.contact, name= "contact"),
 ]
